9edbcdf168c63e1525bb27b3c29a2ee67f6a7385
Anil <gugulothuanilkumar09@gmail.com>
2025-12-29 21:09:47 +0530

    Added path for openAI LLM model

---
+++ app/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI,Query
+from openai import OpenAI
+
+from typing import Annotated
+
+
+app = FastAPI()
+openai_client = OpenAI(api_key="openAI_api_key")
+
+
+@app.get("/health-check")
+def health_check_service():
+    return {"status": "healthy"}
+
+
+@app.get("/api/chat")
+def chat_controller(
+    model_name : Annotated[str,Query(title = "Gen AI model name")],
+    prompt: Annotated[str,Query(title = 'Prompt to the GenAI model')] = "Inspire me"):
+
+    response = openai_client.chat.completions.create(
+        model=model_name,
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": prompt},
+        ],
+    )
+    statement = response.choices[0].message.content.strip()
+    return {"statement": statement}
